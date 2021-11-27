a03bee7172993202ec81c214f130c6c995c83b7d
aikidoka17 <ayoub.amkhazzou.me@gmail.com>
2021-11-27 23:34:48 +0100

    cccc

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import  bodyParser from 'body-parser';
 
 import productsRoutes from "./routes/products.js";
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
